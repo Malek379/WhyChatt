dff7be36502db18991356388efcaf07f0d125390
Malek379 <76846696+Malek379@users.noreply.github.com>
2023-09-23 21:46:56 +0300

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,52 @@
+// Mongoose
+import mongoose from "mongoose";
 
+// MongoURI
+const uri = "mongodb+srv://whyalphadash:w0snFuNXwaKDquuA@chatdb.ppkbjco.mongodb.net/?retryWrites=true&w=majority";
+
+// Bağlanma
+mongoose.connect(uri);
+
+// Kayıt()
+function kayit() {
+  
+  // Değişkenler
+  if (typeof document === 'undefined') {
+    const uname = document.getElementById("username").value;
+    const upass = document.getElementById("username").value;
+    const umail = document.getElementById("username").value;
+    
+    const userSchema = new mongoose.Schema({
+      UserName: {
+        type: String,
+        required: true,
+      },
+      UserPassword: {
+        type: String,
+        required: true,
+      },
+      UserEmail: {
+        type: String,
+        required: true,
+      },
+    });
+    
+    const newSchema = new userSchema({
+      UserName: uname,
+      UserPassword: upass,
+      UserEmail: umail,
+    });
+    
+    const userData = mongoose.model("KullanıcıVerisi", newSchema);
+    
+    userData.save();
+    
+  }
+}
+
+// Giriş
+function giris() {
+  
+  // Henüz Hazır Değil
+  
+}
